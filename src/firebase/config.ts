3a4bb8c6aa18b138eea10cf0de500a310837ca1f
Use Me As a Tool <fackid20222022@gmail.com>
2025-11-08 13:51:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9920209328-4255e",
+  "appId": "1:380826170979:web:e7300ca1fc97edd5edaa56",
+  "apiKey": "AIzaSyCgBNok5wCmvW9XpCrQWW5cBNpH8EydD1c",
+  "authDomain": "studio-9920209328-4255e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "380826170979"
+};
