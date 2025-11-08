eb45e4cde1cfe065b6be475849967f66ef9b2d94
Use Me As a Tool <fackid20222022@gmail.com>
2025-11-08 14:44:50 +0000

    Login failed: FirebaseError: Firebase: Error (auth/api-key-not-valid.-pl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9920209328-4255e",
-  "appId": "1:380826170979:web:e7300ca1fc97edd5edaa56",
-  "apiKey": "AIzaSyCgBNok5wCmvW9XpCrQWW5cBNpH8EydD1c",
-  "authDomain": "studio-9920209328-4255e.firebaseapp.com",
-  "measurementId": "",
+  "projectId": "expensewise-a2a43",
+  "appId": "1:380826170979:web:122978038318289456570c",
+  "storageBucket": "expensewise-a2a43.appspot.com",
+  "apiKey": "AIzaSyAz_-_y0t4p4E-nJm3t5p7b1b3q0t1r9r8",
+  "authDomain": "expensewise-a2a43.firebaseapp.com",
   "messagingSenderId": "380826170979"
 };
